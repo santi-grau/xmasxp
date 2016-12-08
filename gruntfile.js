98d7019246b9a0c08f1101a4d9aeec080e260e12
Santi Grau <sgrautorredemer@apple.com>
2016-12-08 14:00:58 -0800

    added build scripts : npm run build

---
+++ gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    clean: {
+      build: {
+        files: [{
+          dot: true,
+          src: [
+            "build/*"
+          ]
+        }]
+      }
+    },
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: "app",
+          src: [
+            "assets/**/*",
+            "js/scripts/**/*"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+//    browserify: {
+//      dist: {
+//        files: {
+//          'build/js/main.js': ['app/js/main.js']
+//        },
+//        options: {
+//          // transform: [
+//          //   function(file) {
+//          //     return stringify({extensions: ['.obj']}).call(stringify, file);
+//          //   }
+//          // ]
+//          transform: [
+//            [
+//              'stringify',
+//              {
+//                appliesTo: { includeExtensions: [ '.obj' ] }
+//              }
+//            ]
+//          ]
+//        }
+//      }
+//    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false,
+            page: "main"
+          }
+        },
+        files: {
+          "build/index.html": "app/views/main.jade"
+        }
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          paths: ['public/css/main.styl'],
+          use: [
+            require('nib') // use stylus plugin at compile time
+          ]
+        },
+        files: {
+          'build/css/main.css': 'app/css/main.styl'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-browserify');
+  
+  // Build Tasks
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'jade',
+    //'browserify',
+    'stylus'
+  ]);
+};
